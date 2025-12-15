863a65ab90aae21524b0e130a446fe4dfc212e9a
Mtvare10 <nikamtvarelishvili@gmail.com>
2025-12-15 15:40:47 +0400

    modified cache and main.py

--- main.py
+++ main.py
@@ -3,13 +3,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 
 def index():
     return {"data": {"name" : "nika"}}
 
-@app.get("/about")
-def about():
-    return {"data" : "about page"}
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"data": "all unpublished data"}
+
+@app.get("/blog/{id}")
+def show(id: int):
+    return {"data" : id}
+
+@app.get("/blog/{id}/comments")
+def comments(id: int):
+    return {"data" : {"1", "2", "3"}}
+
 
 
