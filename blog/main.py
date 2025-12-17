0a1898953e7765b274a51774a4172f70fb2fed21
Mtvare10 <nikamtvarelishvili@gmail.com>
2025-12-17 16:41:46 +0400

    udate blog.db and blog main.py, added get, put and delete methods

--- blog/main.py
+++ blog/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from . import models, schemas
-from .database import engine
-
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
+from fastapi import Depends, status
 
 app = FastAPI()
 
@@ -9,10 +10,30 @@ app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
 
-@app.post("/blog")
-def create_blog(request : schemas.Blog, db):
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/blog", status_code=status.HTTP_201_CREATED)
+def create_blog(request : schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+
+@app.get("/blog")
+def all(db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).all()
+    return blog
+
+@app.get("/blog/{id}", status_code=status.HTTP_200_OK)
+def show(id: int, response: Response, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")   
+    return blog
